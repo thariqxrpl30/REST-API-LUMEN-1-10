56a5c9e2fad4d9440a3dd08cd834cb5de1780d34
ThariqXIRPL29 <87298575+thariqxrpl30@users.noreply.github.com>
2022-01-31 08:41:22 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group( ['prefix' => 'api'] , function() use ($router) {
+
+    $router->get('kategori' , ['uses' => 'KategoriController@index']);
+
+    $router->get('kategori/{id}' , ['uses' => 'KategoriController@show']);
+
+    $router->delete('kategori/{id}' , ['uses' => 'KategoriController@destroy']); //ini utk hapus data
+
+    $router->put('kategori/{id}' , ['uses' => 'KategoriController@Update']); //update pake put
+
+    $router->post('kategori' , ['uses' => 'KategoriController@create']); //ini utk inset data
+
+    $router->get('pelanggan' , ['uses' => 'PelangganController@index']);
+
+    $router->get('pelanggan/{id}' , ['uses' => 'PelangganController@show']); //ini utk tampilkan data
+
+    $router->post('pelanggan' , ['uses' => 'PelangganController@create']);
+
+    $router->delete('pelanggan/{id}' , ['uses' => 'PelangganController@destroy']);
+
+} );
+
+
